55ef7ea3f080b61459d176c780adafd3660a0fca
mesud_17 <mesud3818@gmail.com  git config --global user.email mesud3818@gmail.com  git config --global user.email mesud3818@gmail.com>
2026-04-30 14:06:07 +0300

    adding new componenet

--- react-apple/src/components/FifthSection/FifthSection.jsx
+++ react-apple/src/components/FifthSection/FifthSection.jsx
@@ -1,42 +1,43 @@
 
 import tv_logo from "../../assets/images/icons/apple-tv-logo.png"
 import watch_logo from "../../assets/images/icons/watch-series5-logo.png"
+import banker from "../../assets/images/home/banker.png"
 
 function FifthSection() {
   return (
     <>
-    <section class="fifth-heghlight-wrapper">
-		<div class="container-fluid">
-			<div class="row">
-				<div class="left-side-wrapper col-sm-12 col-md-6">
-					<div class="left-side-container">
-						<div class="top-logo-wrapper">
-							<div class="logo-wrapper">
+    <section className="fifth-heghlight-wrapper">
+		<div className="container-fluid">
+			<div className="row">
+				<div className="left-side-wrapper col-sm-12 col-md-6">
+					<div className="left-side-container">
+						<div className="top-logo-wrapper">
+							<div className="logo-wrapper">
 								<img src={tv_logo} alt="Apple TV Logo"/>
 							</div>
 						</div>
 
-						<div class="tvshow-logo-wraper">
-							<img src="images/home/banker.png" alt="Banker Logo"/>
+						<div className="tvshow-logo-wraper">
+							<img src={banker} alt="Banker Logo"/>
 						</div>
 
-						<div class="watch-more-wrapper">
+						<div className="watch-more-wrapper">
 							<a href="#">Watch now on the Apple TV App</a>
 						</div>
 					</div>
 				</div>
-				<div class="right-side-wrapper col-sm-12 col-md-6">
-					<div class="right-side-container">
-						<div class="top-logo-wrapper">
-							<div class="logo-wrapper">
+				<div className="right-side-wrapper col-sm-12 col-md-6">
+					<div className="right-side-container">
+						<div className="top-logo-wrapper">
+							<div className="logo-wrapper">
 								<img src={watch_logo} alt="Apple Watch Logo"/>
 							</div>
 						</div>
-						<div class="description-wraper">
+						<div className="description-wraper">
 							With the Always-On Retina display.<br/>
 							You’ve never seen a watch like this.
 						</div>
-						<div class="links-wrapper">
+						<div className="links-wrapper">
 							<ul>
 								<li><a href="">Learn more</a></li>
 								<li><a href="">Buy</a></li>
